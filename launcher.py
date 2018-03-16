1ca2847f57265657082a8ac8649a4cd99ac97538
Joonyoung Yi <joonyoung.yi@kaist.ac.kr>
2018-03-16 17:03:38 +0900

    일단 완성 하지만, memory 부족

--- launcher.py
+++ launcher.py
@@ -18,23 +18,51 @@ MAX_MOVIE_ID = 131263
 f = open(filename, 'r')
 f.readline()
 
-m = np.zeros((MAX_USER_ID, MAX_MOVIE_ID))
+uid2rid_dict, mid2cid_dict = {}, {}
+rid, cid = 0, 0
 for line in f:
     cols = line.split(',')
+    uid = int(cols[0])
+    mid = int(cols[1])
+    r = int(float(cols[2]) * 2)
 
-    user_id = int(cols[0])
-    movie_id = int(cols[1])
-    rating = int(float(cols[2]) * 2)
+    r_index = uid2rid_dict.get(uid, None)
+    if r_index is None:
+        uid2rid_dict[uid] = rid
+        rid += 1
 
-    m[user_id, movie_id] = rating
+    c_index = mid2cid_dict.get(mid, None)
+    if c_index is None:
+        mid2cid_dict[mid] = cid
+        cid += 1
 
-print(m.shape)
-m = m[~np.all(m == 0, axis=1)]
-print(m.shape)
-m = m[~np.all(m == 0, axis=2)]
-print(m.shape)
+print(rid, cid)
+truth = np.zeros((rid, cid))  # answer
 
-# alternating_minimization = AlternatingMinimization()
-# r, a = alternating_minimization.run(m)
-# error = np.linalg.norm(np.subtract(m, r), ord='fro')
-# print(error)
+f = open(filename, 'r')
+f.readline()
+
+for line in f:
+    cols = line.split(',')
+    uid = int(cols[0])  # user id
+    mid = int(cols[1])  # movie id
+    score = int(float(cols[2]) * 2)  # score
+
+    rid = uid2rid_dict[uid]  # row id
+    cid = mid2cid_dict[mid]  # column id
+
+    truth[rid, cid] = score
+
+mask = np.random.randint(1, 10, truth.shape)
+train = np.copy(truth)  # training matrix
+train[mask == 1] = 0
+test = np.copy(truth)  # test matrix
+test[mask != 1] = 0
+
+alternating_minimization = AlternatingMinimization()
+answer, left = alternating_minimization.run(train)
+error = np.linalg.norm(np.subtract(answer, test)[mask == 1], ord='fro')
+print(error)
+count = count_nonzero(test)
+print(count)
+print(error / count)
