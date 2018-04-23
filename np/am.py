e1ea41550b7c1614ace1ef4b4dfaf8a4d0dbdd13
Joonyoung Yi <joonyoung.yi@kaist.ac.kr>
2018-04-23 15:25:46 +0900

    complete numpy version of am

--- np/am.py
+++ np/am.py
@@ -1,162 +1,83 @@
+import math
+
 import numpy as np
 
 
 class AlternatingMinimization(object):
-    """ this class is for performing low rank matrix completion
-
-    Attributes:
-      _MAX_ERROR (float): default error boundary
-
     """
-    _MAX_ERROR = 1e10
+    this class is for performing low rank matrix completion
+    """
 
     @classmethod
-    def _proper_max(cls, row_x, col_x, rank, max_rank):
+    def _get_V_from_U(cls, M, U, mask):
         """
-        determine proper max value
+        compute matrix V from original matrix M and left matrix U.
 
         Args:
-            row_x (int): number of rows of given matrix
-            col_x (str): number of columns of given matrix
-            rank (int): number of singular values
-            max_rank (str): user-provided maximum limit
-
-        Returns:
-            int: proper max rank
-        """
-        return min(rank, max_rank, int(min(row_x, col_x) / 10))
-
-    @classmethod
-    def _get_r_from_l(cls, matrix_x, matrix_l):
-        """
-        compute matrix R from original matrix and left matrix.
-
-        Args:
-            matrix_x (numpy.array): given matrix
-            matrix_l (numpy.array): left matrix
+            M (numpy.array): given matrix
+            U (numpy.array): left matrix
+            mask (numpy.array): mask of hidden entries
 
         Returns:
             numpy.array: right matrix
         """
-        column = matrix_x.shape[1]
-        rank = matrix_l.shape[1]
-        R = np.empty((rank, column), dtype=np.float16)
-        for j in range(0, column):
-            matrix_l_new = matrix_l.copy()
-            matrix_l_new[matrix_x[:, j] == 0, :] = 0
-
-            R[:, j] = np.dot(np.linalg.pinv(matrix_l_new), matrix_x[:, j])
-        return R
-
-    @classmethod
-    def _get_r_from_l2(cls, matrix_x, matrix_l):
-        """
-        compute matrix R from original matrix and left matrix.
-
-        Args:
-            matrix_x (numpy.array): given matrix
-            matrix_l (numpy.array): left matrix
-
-        Returns:
-            numpy.array: right matrix
-        """
-        column = matrix_x.shape[1]
-        rank = matrix_l.shape[1]
-        matrix_r = np.empty((rank, column))
+        column = M.shape[1]
+        rank = U.shape[1]
+        V = np.empty((rank, column), dtype=type(M))
 
         for j in range(0, column):
-            matrix_l_new = matrix_l.copy()
-            matrix_l_new[matrix_x[:, j] == 0, :] = 0
-
-            q_class = np.dot(matrix_l_new.T, matrix_l_new)
-            c_class = np.dot(matrix_x[:, j], matrix_l_new)
-            q_det = np.linalg.det(q_class)
-            if q_det < 1e-10:
-                matrix_r[:, j] = np.dot(
-                    np.linalg.pinv(q_class, 1e-10), c_class)
-            else:
-                matrix_r[:, j] = np.linalg.solve(q_class, c_class)
-        return matrix_r
+            U_ = U.copy()
+            U_[mask[:, j], :] = 0
+            V[:, j] = np.linalg.lstsq(U_, M[:, j], rcond=None)[0]
+        return V
 
     @classmethod
-    def _get_err(cls, matrix_x, matrix_y):
+    def _get_err(cls, M, U, V, mask):
         """
         compute training error
 
         Args:
-            matrix_x (numpy.array): original matrix
-            matrix_y (numpy.array): derived matrix
+            M (numpy.array): original matrix
+            U (numpy.array): derived left matrix
+            V (numpy.array): derived right matrix
+            mask (numpy.array): mask of hidden entries
 
         Returns:
             float: training error
         """
-        error_matrix = matrix_x - matrix_y
-        error_matrix[matrix_x == 0] = 0
-        return np.linalg.norm(error_matrix, 'fro') / np.count_nonzero(matrix_x)
+        error_matrix = M - np.dot(U, V)
+        error_matrix[mask] = 0
+        return np.linalg.norm(error_matrix, 'fro') / np.count_nonzero(mask)
 
     @classmethod
-    def _get_candidate(cls, matrix_x, matrix_l_rank_k, delta, max_iter):
-        """
-        get matrix_l of rank k and candidate matrix from given matrix_x and initial matrix_l of rank k
-
-        Args:
-            matrix_x (int): given matrix
-            matrix_l_rank_k (str): matrix l that has k number of columns
-            delta (int): training error gap for stop training if error is close enough
-            max_iter (str): maximum number of iteration
-
-        Returns:
-            float: Description of return value
-            numpy.array: best left side matrix that can express matrix_x
-            numpy.array: best right side matrix that can express matrix_x
-        """
-        result = (cls._MAX_ERROR, None, None)
-
-        for _ in range(0, max_iter):
-            matrix_r_rank_kt = cls._get_r_from_l2(matrix_x, matrix_l_rank_k)
-            matrix_l_rank_k = cls._get_r_from_l2(matrix_x.T,
-                                                 matrix_r_rank_kt.T).T
-            candidate = np.dot(matrix_l_rank_k, matrix_r_rank_kt)
-            train_err = cls._get_err(matrix_x, candidate)
-
-            if -delta < result[0] - train_err < delta:
-                result = (train_err, matrix_l_rank_k, matrix_r_rank_kt)
-                break
-            if result[0] > train_err:
-                result = (train_err, matrix_l_rank_k, matrix_r_rank_kt)
-        return result
-
-    @classmethod
-    def run(cls, matrix_x, min_rank=1, max_rank=30, delta=1e-6,
-            max_iter=10000):
+    def run(cls, M, mask, k=1, early_stop=1e-6, delta=1e-6, max_iter=10000):
         """
         perform low rank matrix completion
 
         Args:
-            matrix_x (int): given matrix
-            min_rank (str): minimum rank to try
-            max_rank (int): maximum rank to try
-            delta (str): training/test error gap for stop if error is close enough
+            M (numpy.array): given matrix
+            mask (numpy.array): mask of hidden entries
+            rank (int): rank to try
+            early_stop (float): early stop criteria for training error.
             max_iter (int): maximum number of iterations per each rank
 
         Returns:
-            numpy.array: best left side matrix that can express matrix_x
-            numpy.array: best right side matrix that can express matrix_x
+            numpy.array: best left side matrix(U) that can express matrix M
+            numpy.array: best right side matrix(V) that can express matrix M
         """
-        matrix_u, matrix_sigma, matrix_vt = np.linalg.svd(
-            matrix_x, full_matrices=False)
-        rank = cls._proper_max(matrix_x.shape[0], matrix_x.shape[1],
-                               matrix_sigma.shape[0], max_rank)
-        min_rank = min(min_rank, rank)
-        result = (cls._MAX_ERROR, None, None)
+        train_err = None
+        U, S, V = np.linalg.svd(M, full_matrices=False)
+        U, V, train_err = U[:, :k], np.dot(S, V), None
 
-        for k in range(min_rank, rank + 1):
-            candidate_tuple = cls._get_candidate(
-                matrix_x, matrix_u[:, :k].copy(), delta, max_iter)
-            # self.log.info('RANK : %d \t(Training Error : %f)' % (k, err))
-            if result[0] - candidate_tuple[0] > delta:
-                result = candidate_tuple
-            else:
+        for _ in range(max_iter):
+            V = cls._get_V_from_U(M, U, mask)
+            U = cls._get_V_from_U(M.T, V.T, mask.T).T
+
+            err = cls._get_err(M, U, V, mask)
+            print('>>', err)
+            if err < early_stop:
                 break
-
-        return result[1], result[2]
+            if train_err and (-delta < train_err - err < delta):
+                break
+            train_err = err
+        return train_err, U, V
--- np/run.py
+++ np/run.py
@@ -1,18 +1,28 @@
+import random
+
 import numpy as np
 
 from am import AlternatingMinimization as AM
 
 m = 100
-n = 20
+n = 100
 k = 2
+p = 0.5
 
 L = np.random.randn(m, k)
 R = np.random.randn(k, n)
 M = np.dot(L, R)
 
+M_max = np.max(M)
+M_min = np.min(M)
+
+M_ = M.copy()
+mask = np.random.rand(m, n) >= p
+M_[mask] = 0
+
 am = AM()
-U, V = am.run(M, min_rank=k, max_rank=k)
-X = np.matmul(U, V)
+train_err, U, V = am.run(M_, k=k, mask=mask)
+X = np.dot(U, V)
 error = np.linalg.norm(
     np.subtract(M, X), ord='fro') / np.linalg.norm(
         M, ord='fro')
